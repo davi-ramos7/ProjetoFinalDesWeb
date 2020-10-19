45c41933d5873e8d66834248ffedb7fa21224648
davi-ramos72 <72760642+davi-ramos7@users.noreply.github.com>
2020-10-19 00:39:50 -0300

    Alterações feitas no dia 18/10/2020.

--- estoque_ver.php
+++ estoque_ver.php
@@ -1,1 +1,20 @@
-Teste estoque
+<div class="fundo-estoque">
+    <h1 class="titulo" >Ver Estoque e Preço</h1>
+    <button id="btn_bscClientes">Preencher</button><br>
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>NOME</th>
+            <th>SOBRENOME</th>
+            <th>CPF</th>
+            <th>ENDERECO</th>
+            <th>TELEFONE</th>
+            <th>EMAIL</th>
+        </tr>
+    </thead>
+    <tbody id="tblClientes">
+
+    </tbody>
+</table>
+</div>
---
+++ estoque_ver_dk.php
@@ -0,0 +1,18 @@
+<div class="fundo-estoque">
+    <!-- <button id="btn_bscClientes">Preencher</button><br> -->
+    <table>
+        <thead>
+            <tr>
+                <th>Fabricante</th>
+                <th>Processador</th>
+                <th>Memória Ram</th>
+                <th>Hd</th>
+                <th>Preço</th>
+                <th>Estoque</th>
+            </tr>
+        </thead>
+        <tbody id="tblDk">
+
+        </tbody>
+    </table>
+</div>
